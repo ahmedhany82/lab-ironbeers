33f001ec6d8ca841dab971184a76cc5ee58fca12
Ahmed Abdelsalam <ahmedabdelsalam82.de@gmail.com>
2021-02-04 15:07:20 +0100

    done with non-bonuse iterations

--- app.js
+++ app.js
@@ -22,4 +22,18 @@ app.get('/', (req, res) => {
   res.render('index');
 });
 
+app.get('/beers', (req, res) => {
+  punkAPI.getBeers()
+    .then(beersFromApi => res.render('beers', {beersList: beersFromApi}))
+    .catch(error => console.log(error)
+    );
+});
+
+app.get('/random-beers', (req, res) => {
+  punkAPI.getRandom()
+    .then(responseFromAPI => res.render('random-beer', {random: responseFromAPI}))
+    .catch(error => console.log(error));
+});
+
+
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
